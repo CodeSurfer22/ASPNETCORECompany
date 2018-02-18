1a608e20e3a347f55e70094e13c77bfc1182c9ea
CodeSurfer22 <34723197+CodeSurfer22@users.noreply.github.com>
2018-02-19 00:24:08 +0200

    ASP.NETCOREMVCCRUD
    ASP.NETCOREMVCCRUD

---
+++ ASPNETCORECompany/Data/EmployeeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETCORECompany.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext (DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASPNETCORECompany.Models.Employee> Employee { get; set; }
+        public DbSet<ASPNETCORECompany.Models.Manager> Manager { get; set; }
+    }
+}
---
+++ ASPNETCORECompany/Data/ManagerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETCORECompany.Models
+{
+    public class ManagerContext : DbContext
+    {
+        public ManagerContext (DbContextOptions<ManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASPNETCORECompany.Models.Manager> Manager { get; set; }
+    }
+}
